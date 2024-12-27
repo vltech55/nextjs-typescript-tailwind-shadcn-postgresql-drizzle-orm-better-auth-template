181c9ccaa1fc7a73ccfb7c263db8883ad83cc895
Vlad L. <218786589+vltech55@users.noreply.github.com>
2024-12-28 01:27:28 +0530

    feat: refactor font handling and improve styling
    Replace Google Fonts with local font imports for Raleway. Update 
    global styles to enhance color variables and improve dark mode 
    support. Adjust body font-family and apply Tailwind CSS utilities 
    for consistent styling across components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import "../styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {
+  R,
+  RI,
+} from "@/styles/Fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${R.variable} ${RI.variable} antialiased`}
       >
         {children}
       </body>
