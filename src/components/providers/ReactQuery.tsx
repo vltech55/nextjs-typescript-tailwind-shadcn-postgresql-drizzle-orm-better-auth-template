97ebdd26da9bc8da41f8e05cd341440a32ba59b0
Vlad L. <218786589+vltech55@users.noreply.github.com>
2025-01-04 20:34:49 +0530

    feat: add React Query provider to layout
    Introduce a new ReactQueryProvider component to manage 
    React Query's state and caching. Wrap the main layout 
    in the provider to ensure all child components can access 
    the query client. Include React Query Devtools for 
    development environment to aid in debugging. Update 
    import paths for consistency and clarity.

---
+++ src/components/providers/ReactQuery.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ReactQueryProvider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
