7e1c1877aabc6d3bcc49f1a32b10c789978f7e3b
Vlad L. <218786589+vltech55@users.noreply.github.com>
2025-03-10 20:59:42 +0530

    Converts Context.Provider JSX opening and closing elements into Context.

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -36,9 +36,9 @@ const FormField = <
   ...props
 }: ControllerProps<TFieldValues, TName>) => {
   return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
+    (<FormFieldContext value={{ name: props.name }}>
       <Controller {...props} />
-    </FormFieldContext.Provider>
+    </FormFieldContext>)
   );
 };
 
@@ -80,9 +80,9 @@ const FormItem = React.forwardRef<
   const id = React.useId();
 
   return (
-    <FormItemContext.Provider value={{ id }}>
+    (<FormItemContext value={{ id }}>
       <div ref={ref} className={cn("space-y-2", className)} {...props} />
-    </FormItemContext.Provider>
+    </FormItemContext>)
   );
 });
 FormItem.displayName = "FormItem";
